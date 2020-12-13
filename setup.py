524a54335f6cf2f8e562c01906b806f30e966165
Sudhanshu1304 <sudhanshu.dpandey@gmail.com>
2020-12-13 14:16:41 +0530

    Updated the setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ classifiers = [
   'Intended Audience :: Devlopers',
   'Operating System :: Microsoft :: Windows :: Windows 10',
   'License :: OSI Approved :: MIT License',
-  'Programming Language :: Python :: 3'
+  'Programming Language :: Python :: 3',
   'Programming Language :: Python :: 3.0',
   'Programming Language :: Python :: 3.3',
   'Programming Language :: Python :: 3.4',
   'Programming Language :: Python :: 3.5',
-  'Programming Language :: Python :: 3.6'
+  'Programming Language :: Python :: 3.6',
   'Programming Language :: Python :: 3.7',
   'Programming Language :: Python :: 3.8',
   'Programming Language :: Python :: 3.9'
@@ -20,7 +20,7 @@ classifiers = [
 setup(
   name='ModelAuto',
   packages=['ModelAuto'],
-  version='0.0.1',
+  version='0.1',
   
   description='Speed up your model making process. This will help you in selecting best features, best Models (SVM,SVR Random Fotest e.t.c and also in Data Preprocessing',
   

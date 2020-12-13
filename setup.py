1ac742584632687748fe888fe227344871cfbfe1
Sudhanshu1304 <sudhanshu.dpandey@gmail.com>
2020-12-13 14:34:42 +0530

    Updating setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 classifiers = [
   'Development Status :: 3 - Alpha',
   'Topic :: Scientific/Engineering :: Artificial Intelligence',
-  'Intended Audience :: Devlopers',
+  'Intended Audience :: Education',
   'Operating System :: Microsoft :: Windows :: Windows 10',
   'License :: OSI Approved :: MIT License',
   'Programming Language :: Python :: 3',
